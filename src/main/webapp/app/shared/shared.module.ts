2bb7b3fd2e7388babedb7c871dd0bf2c88c0e707
Leandro Jácome <leandro.jacome@gmail.com>
2024-02-02 23:53:06 -0300

    Adicionar o módulo FontAwesome ao SharedModule e importar pacote de ícones sólidos.

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 /**
  * Application wide Module
  */
 @NgModule({
-  imports: [AlertComponent, AlertErrorComponent],
+  imports: [AlertComponent, AlertErrorComponent, FontAwesomeModule],
   exports: [CommonModule, NgbModule, FontAwesomeModule, AlertComponent, AlertErrorComponent],
+  declarations: [],
 })
-export default class SharedModule {}
+export default class SharedModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
